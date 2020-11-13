9b0bdb2a1339a3c13563ea1e6da99d668dc8cac6
wise-king-sullyman <a.m.sullivan.github@gmail.com>
2020-11-12 23:52:35 -0500

    Add King #clean_moves to prevent king from moving into check

--- lib/piece.rb
+++ lib/piece.rb
@@ -55,6 +55,11 @@ class King
 
   private
 
+  def clean_moves(moves)
+    super
+    moves.delete_if { |move| @game.in_check_at(move) }
+  end
+
   def possible_moves(row, column)
     modifiers = [1, 0, -1].product([1, 0, -1])
     moves = apply_move_modifiers(modifiers, row, column)
