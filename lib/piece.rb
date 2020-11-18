4d0c18468dd6552987db9547a9eb6b7ecc5cbbad
wise-king-sullyman <a.m.sullivan.github@gmail.com>
2020-11-17 22:30:24 -0500

    Add modified versions of various methods to Pawn
    Add modified versions of various methods to Pawn to fix bug in pawn not detecting it can
    check the king if the king moves into the pawns attack path

--- lib/piece.rb
+++ lib/piece.rb
@@ -174,4 +174,29 @@ class Pawn
     moves.push([row + @direction, column - 1]) if @game.enemy_at?(@player, [row + @direction, column - 1])
     clean_moves(moves)
   end
+
+  def can_attack_location?(location)
+    valid_pawn_move?(location)
+  end
+
+  def can_attack_king?
+    valid_pawn_move?(@game.enemy_king_location(@player))
+  end
+
+  def valid_pawn_move?(location)
+    legal_pawn_move?(location) \
+    && @game.available?(@player, location) \
+    && @game.reachable?(self, location)
+  end
+
+  def legal_pawn_move?(move)
+    possible_pawn_attack_moves(@location.first, @location.last).include?(move)
+  end
+
+  def possible_pawn_attack_moves(row, column)
+    moves = []
+    moves.push([row + @direction, column + 1])
+    moves.push([row + @direction, column - 1])
+    clean_moves(moves)
+  end
 end
