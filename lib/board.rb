2c22eb7f85e77f06dae93a94c86726445d4057b4
wise-king-sullyman <a.m.sullivan.github@gmail.com>
2020-12-16 22:37:43 -0500

    Rename Board @game_board to @current_board

--- lib/board.rb
+++ lib/board.rb
@@ -2,39 +2,39 @@
 
 # create/update board and reply if a space is occupied
 class Board
-  attr_reader :players, :empty_tile, :game_board
+  attr_reader :players, :empty_tile, :current_board
 
   def initialize(players)
     @players = players
     @empty_tile = "\u25A1".encode + ' '
-    @game_board = make_blank_board
+    @current_board = make_blank_board
   end
 
   def update(row, column, piece)
-    @game_board[row][column] = piece
+    @current_board[row][column] = piece
   end
 
   def to_s
     board_string = "  a b c d e f g h \n"
-    board_string = draw_rows(game_board, board_string)
+    board_string = draw_rows(current_board, board_string)
     board_string += "  a b c d e f g h \n"
     board_string
   end
 
   def piece_at(location)
-    @game_board[location.first][location.last]
+    @current_board[location.first][location.last]
   end
 
   def refresh
-    @game_board = make_blank_board
+    @current_board = make_blank_board
     players.each do |player|
       player.pieces.each do |piece|
         row = piece.location.first
         column = piece.location.last
-        @game_board[row][column] = piece
+        @current_board[row][column] = piece
       end
     end
-    @game_board
+    @current_board
   end
 
   private
