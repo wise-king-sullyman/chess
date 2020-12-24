76404158f0144c97cbdcdbaa54b47eba5804420a
wise-king-sullyman <a.m.sullivan.github@gmail.com>
2020-12-24 12:11:49 -0500

    Remove Board #to_s testing

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,55 +7,6 @@ describe Board do
   let(:players) { [] }
   subject(:board) { Board.new(players) }
 
-  describe '#to_s' do
-    before do
-      allow($stdout).to receive(:write)
-    end
-
-    context 'when board is blank' do
-      it 'prints a blank board' do
-        output_string =
-          "  a b c d e f g h \n"\
-          "8 □ □ □ □ □ □ □ □  8\n"\
-          "7 □ □ □ □ □ □ □ □  7\n"\
-          "6 □ □ □ □ □ □ □ □  6\n"\
-          "5 □ □ □ □ □ □ □ □  5\n"\
-          "4 □ □ □ □ □ □ □ □  4\n"\
-          "3 □ □ □ □ □ □ □ □  3\n"\
-          "2 □ □ □ □ □ □ □ □  2\n"\
-          "1 □ □ □ □ □ □ □ □  1\n"\
-          "  a b c d e f g h \n"
-        expect do
-          puts board
-        end.to output(output_string).to_stdout
-      end
-    end
-
-    context 'after a piece is added' do
-      let(:player) { instance_double('player') }
-      let(:game) { instance_double('game')}
-      let(:piece) { Pawn.new(game, player, [1, 0]) }
-      it 'prints the piece in place' do
-        allow(player).to receive(:color).and_return('white')
-        board.update(1, 0, piece)
-        output_string =
-          "  a b c d e f g h \n"\
-          "8 □ □ □ □ □ □ □ □  8\n"\
-          "7 ♙ □ □ □ □ □ □ □  7\n"\
-          "6 □ □ □ □ □ □ □ □  6\n"\
-          "5 □ □ □ □ □ □ □ □  5\n"\
-          "4 □ □ □ □ □ □ □ □  4\n"\
-          "3 □ □ □ □ □ □ □ □  3\n"\
-          "2 □ □ □ □ □ □ □ □  2\n"\
-          "1 □ □ □ □ □ □ □ □  1\n"\
-          "  a b c d e f g h \n"
-        expect do
-          puts board
-        end.to output(output_string).to_stdout
-      end
-    end
-  end
-
   describe '#update' do
     it 'adds a piece to the board when requested' do
       board.update(1, 0, 1)
