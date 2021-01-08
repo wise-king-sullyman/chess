b0a4693c94830dd9aafb61e3087a6aa5f4156994
wise-king-sullyman <a.m.sullivan.github@gmail.com>
2021-01-08 18:34:16 -0500

    Set Piece test structure up

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -1,3 +1,15 @@
 # frozen_string_literal: true
 
 require_relative '../lib/pieces/piece'
+
+describe Piece do
+  let(:game) { double('game') }
+  let(:player) { double('player') }
+  let(:location) { [4, 4] }
+  let(:dummy_class) do
+    class DummyClass
+      include Piece
+    end
+  end
+  subject(:piece) { dummy_class.new(game, player, location) }
+end
