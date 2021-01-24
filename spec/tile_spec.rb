b74f17ab54bf2c91fe6804e0ceb1de57c804b0b3
wise-king-sullyman <a.m.sullivan.github@gmail.com>
2021-01-23 20:10:26 -0500

    Add tests for Tile #colorize_text

--- spec/tile_spec.rb
+++ spec/tile_spec.rb
@@ -34,4 +34,30 @@ describe 'Tile' do
       end
     end
   end
+
+  describe '#colorize_text' do
+    context 'when given color is black' do
+      it 'returns the "black" wrapping' do
+        actual_output = tile_tester.colorize_text('', 'black')
+        expected_output = "\e[38;2;0;0;0m\e[0m"
+        expect(actual_output).to eq(expected_output)
+      end
+    end
+
+    context 'when the given color is white' do
+      it 'returns the "white" wrapping' do
+        actual_output = tile_tester.colorize_text('', 'white')
+        expected_output = "\e[38;2;255;243;230m\e[0m"
+        expect(actual_output).to eq(expected_output)
+      end
+    end
+
+    context 'when text is given' do
+      it 'sets the text to the given color' do
+        actual_output = tile_tester.colorize_text('foo', 'white')
+        expected_output = "\e[38;2;255;243;230mfoo\e[0m"
+        expect(actual_output).to eq(expected_output)
+      end
+    end
+  end
 end
