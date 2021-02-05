268b8d0b9d8d7ad742f302cafe928e2c41eed6b4
Sullyman <a.m.sullivan.github@gmail.com>
2021-02-04 23:58:39 -0500

    Add tests for AI #move

--- spec/ai_spec.rb
+++ spec/ai_spec.rb
@@ -3,8 +3,91 @@
 require_relative '../lib/ai'
 
 describe AI do
-  let(:game_double) { instance_double(Game)}
-  subject(:ai_test) { described_class.new('Hal', 'white', game_double) }
+  let(:game) { instance_double(Game) }
+  let(:piece) { instance_double(Piece) }
+  let(:location) { [4, 4] }
+  subject(:ai_test) { described_class.new('Hal', 'white', game) }
+
+  describe '#move' do
+    before do
+      allow(ai_test).to receive(:piece_choice).and_return(piece)
+      allow(ai_test).to receive(:location_choice).and_return(location)
+      allow(ai_test).to receive(:piece_is_mine?).and_return(true)
+      allow(ai_test).to receive(:valid_move?).and_return(true)
+      allow(game).to receive(:board)
+      allow(game).to receive(:move_piece)
+      allow(ai_test).to receive(:promote)
+      allow(piece).to receive(:eligible_for_promotion?)
+    end
+
+    it 'calls #reset_en_passant_vulnerabilities' do
+      expect(ai_test).to receive(:reset_en_passant_vulnerabilities)
+      ai_test.move
+    end
+
+    it 'calls Game #move_piece with the piece and location' do
+      expect(game).to receive(:move_piece).with(piece, location)
+      ai_test.move
+    end
+
+    context 'when a valid piece and move are given' do
+      it 'calls #piece_choice once' do
+        expect(ai_test).to receive(:piece_choice).once
+        ai_test.move
+      end
+
+      it 'calls #location_choice once' do
+        expect(ai_test).to receive(:location_choice).once
+        ai_test.move
+      end
+    end
+
+    context 'when an invalid piece and invalid move are given at first' do
+      it 'calls #piece_choice twice' do
+        allow(ai_test).to receive(:piece_is_mine?).and_return(false, true)
+        expect(ai_test).to receive(:piece_choice).twice
+        ai_test.move
+      end
+
+      it 'calls #location_choice twice' do
+        allow(ai_test).to receive(:valid_move?).and_return(false, true)
+        expect(ai_test).to receive(:location_choice).twice
+        ai_test.move
+      end
+    end
+
+    context 'when a valid piece is given but an invalid move is given at first' do
+      it 'calls #location_choice twice' do
+        allow(ai_test).to receive(:valid_move?).and_return(false, true)
+        expect(ai_test).to receive(:location_choice).twice
+        ai_test.move
+      end
+    end
+
+    context 'when an invalid piece is given but a valid move is given at first' do
+      it 'calls #piece_choice twice' do
+        allow(ai_test).to receive(:piece_is_mine?).and_return(false, true)
+        expect(ai_test).to receive(:piece_choice).twice
+        ai_test.move
+      end
+    end
+
+    context 'when a piece is eligible for promotion' do
+      it 'calls promote with that piece' do
+        allow(piece).to receive(:eligible_for_promotion?).and_return(true)
+        expect(ai_test).to receive(:promote).with(piece)
+        ai_test.move
+      end
+    end
+
+    context 'when a piece is not eligible for promotion' do
+      it 'calls promote with that piece' do
+        allow(piece).to receive(:eligible_for_promotion?).and_return(false)
+        expect(ai_test).not_to receive(:promote)
+        ai_test.move
+      end
+    end
+  end
 
   describe '#random_tile' do
     before do
