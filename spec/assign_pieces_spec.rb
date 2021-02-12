7d7f8d0a7b40a9d8e6f828fc76b44db96514d73f
Sullyman <a.m.sullivan.github@gmail.com>
2021-02-11 23:25:27 -0500

    Add tests for AssignPieces #assign_pawns

--- spec/assign_pieces_spec.rb
+++ spec/assign_pieces_spec.rb
@@ -282,4 +282,125 @@ describe 'AssignPieces' do
       end
     end
   end
+
+  describe '#assign_pawns' do
+    it 'adds two items to the provided pieces array' do
+      expect { piece_assigner.assign_pawns(player, game, pieces) }.to change { pieces.size }.by(8)
+    end
+
+    it 'only adds pawns to pieces' do
+      piece_assigner.assign_pawns(player, game, pieces)
+      expect(pieces.all? { |piece| piece.instance_of?(Pawn) }).to be(true)
+    end
+
+    context 'when the player is white' do
+      let(:expected_first_location) { [6, 0] }
+      let(:expected_second_location) { [6, 1] }
+      let(:expected_third_location) { [6, 2] }
+      let(:expected_fourth_location) { [6, 3] }
+      let(:expected_fifth_location) { [6, 4] }
+      let(:expected_sixth_location) { [6, 5] }
+      let(:expected_seventh_location) { [6, 6] }
+      let(:expected_eighth_location) { [6, 7] }
+
+      before do
+        allow(player).to receive(:color).and_return('white')
+      end
+
+      it 'assign the expected first location to the first pawn' do
+        piece_assigner.assign_pawns(player, game, pieces)
+        expect(pieces[0].location).to eq(expected_first_location)
+      end
+
+      it 'assign the expected second location to the second pawn' do
+        piece_assigner.assign_pawns(player, game, pieces)
+        expect(pieces[1].location).to eq(expected_second_location)
+      end
+
+      it 'assign the expected third location to the third pawn' do
+        piece_assigner.assign_pawns(player, game, pieces)
+        expect(pieces[2].location).to eq(expected_third_location)
+      end
+
+      it 'assign the expected fourth location to the fourth pawn' do
+        piece_assigner.assign_pawns(player, game, pieces)
+        expect(pieces[3].location).to eq(expected_fourth_location)
+      end
+
+      it 'assign the expected fifth location to the fifth pawn' do
+        piece_assigner.assign_pawns(player, game, pieces)
+        expect(pieces[4].location).to eq(expected_fifth_location)
+      end
+
+      it 'assign the expected sixth location to the sixth pawn' do
+        piece_assigner.assign_pawns(player, game, pieces)
+        expect(pieces[5].location).to eq(expected_sixth_location)
+      end
+
+      it 'assign the expected seventh location to the seventh pawn' do
+        piece_assigner.assign_pawns(player, game, pieces)
+        expect(pieces[6].location).to eq(expected_seventh_location)
+      end
+
+      it 'assign the expected eighth location to the eighth pawn' do
+        piece_assigner.assign_pawns(player, game, pieces)
+        expect(pieces[7].location).to eq(expected_eighth_location)
+      end
+    end
+
+    context 'when the player is black' do
+      let(:expected_first_location) { [1, 0] }
+      let(:expected_second_location) { [1, 1] }
+      let(:expected_third_location) { [1, 2] }
+      let(:expected_fourth_location) { [1, 3] }
+      let(:expected_fifth_location) { [1, 4] }
+      let(:expected_sixth_location) { [1, 5] }
+      let(:expected_seventh_location) { [1, 6] }
+      let(:expected_eighth_location) { [1, 7] }
+
+      before do
+        allow(player).to receive(:color).and_return('black')
+      end
+
+      it 'assign the expected first location to the first pawn' do
+        piece_assigner.assign_pawns(player, game, pieces)
+        expect(pieces[0].location).to eq(expected_first_location)
+      end
+
+      it 'assign the expected second location to the second pawn' do
+        piece_assigner.assign_pawns(player, game, pieces)
+        expect(pieces[1].location).to eq(expected_second_location)
+      end
+
+      it 'assign the expected third location to the third pawn' do
+        piece_assigner.assign_pawns(player, game, pieces)
+        expect(pieces[2].location).to eq(expected_third_location)
+      end
+
+      it 'assign the expected fourth location to the fourth pawn' do
+        piece_assigner.assign_pawns(player, game, pieces)
+        expect(pieces[3].location).to eq(expected_fourth_location)
+      end
+
+      it 'assign the expected fifth location to the fifth pawn' do
+        piece_assigner.assign_pawns(player, game, pieces)
+        expect(pieces[4].location).to eq(expected_fifth_location)
+      end
+
+      it 'assign the expected sixth location to the sixth pawn' do
+        piece_assigner.assign_pawns(player, game, pieces)
+        expect(pieces[5].location).to eq(expected_sixth_location)
+      end
+
+      it 'assign the expected seventh location to the seventh pawn' do
+        piece_assigner.assign_pawns(player, game, pieces)
+        expect(pieces[6].location).to eq(expected_seventh_location)
+      end
+
+      it 'assign the expected eighth location to the eighth pawn' do
+        piece_assigner.assign_pawns(player, game, pieces)
+        expect(pieces[7].location).to eq(expected_eighth_location)
+      end
+    end
+  end
 end
